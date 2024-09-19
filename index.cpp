322c93bf68940f32326a39f3e89cf8357f97a8d3
Joey Peterson <joey.peterson15@gmail.com>
2024-09-19 08:47:12 -0700

    update reverb. length of audio

--- index.cpp
+++ index.cpp
@@ -9,6 +9,8 @@ const int harmonic = 20;
 const float f4Hz = 349.23;
 const int aNote = 440; //hertz
 const int duration = 1; // seconds
+const int N = sampleRate * duration; //sample length
+const int T = N + sampleRate; //Add another second for total length
 
 class SawTooth {
     public:
@@ -35,12 +37,12 @@ class SawTooth {
 
 // y[n] = x[n] + g·y[n−M] = x[n] + feedbackCoefficient·y[n - delay]
 // delay = sampleRate / Fr(resonantFrequency) where Fr = 440 for this project(arbitrary pick)
-std::vector<float> combFilter(std::vector<float> x, float feedbackCoefficient, int delay, int N) {
-    std::vector<float> y(N, 0.0f);
+std::vector<float> combFilter(std::vector<float> x, float feedbackCoefficient, float delay) {
+    std::vector<float> y(T, 0.0f);
     for (int i = 0; i < delay; i++) {
         y[i] = x[i];
     }
-    for (int i = delay; i < N; i++) {
+    for (int i = delay; i < T; i++) {
         y[i] = x[i] + feedbackCoefficient * y[i - delay];
     }
 
@@ -49,36 +51,51 @@ std::vector<float> combFilter(std::vector<float> x, float feedbackCoefficient, i
 
 // y[n] = (−g·x[n]) + x[n−M] + (g·y[n−M])
 // y[n]=−a⋅x[n]+x[n−1]+a⋅y[n−1]
-std::vector<float> allPassFilter(std::vector<float> x, float filterCoefficient, int delay, int N) {
-    std::vector<float> y(N, 0.0f);
+std::vector<float> allPassFilter(std::vector<float> x, float filterCoefficient, int delay) {
+    std::vector<float> y(T, 0.0f);
     for (int i = 0; i < delay; i++) {
         y[i] = x[i];
     }
 
-    for (int i = delay; i < N; i++) {
-        y[i] = (-filterCoefficient * x[i]) + x[i - delay] + (filterCoefficient * y[i - delay]);
+    for (int i = delay; i < T; i++) {
+        y[i] = -filterCoefficient * x[i] + x[i - delay] + (filterCoefficient * y[i - delay]);
     }
+
+        float maxAbsAmp = 0.0f;
+        for (int i = 0; i < T; i++) {
+            if (fabs(y[i] > maxAbsAmp)) {
+                maxAbsAmp = fabs(y[i]);
+            }
+        }
+            // b. divide all values by the max amplitude(if the max amplitude is above 1)
+        if (maxAbsAmp > 1.0f) {
+            for (int i = 0; i < T; i++) {
+                y[i] = y[i] / maxAbsAmp;
+            }
+        }
 
     return y;
 }
 
 int main() {
-    int N = sampleRate * duration;
 
-    SawTooth sawToothA(0.5f, aNote);
-    SawTooth sawToothF(0.5f, f4Hz);
+
+    SawTooth sawToothA(0.25f, aNote);
+    SawTooth sawToothF(0.25f, f4Hz);
 
     // collect samples from sawTooth
-    std::vector<float> audioSamples(N, 0.0f);
+    std::vector<float> audioSamples(T, 0.0f);
     for (int i = 0; i < N; i++) {
         audioSamples[i] = sawToothA.process(harmonic) + sawToothF.process(harmonic);
     }
+    // for (int i = T - (T - N); i < T; i++) {
+    // }
 
     // Comb filter -> delay and gain values are taken from stanford text
-    std::vector<float> combFilterSamples1 = combFilter(audioSamples, 0.742f, 4799, N);
-    std::vector<float> combFilterSamples2 = combFilter(audioSamples, 0.733f, 4999, N);
-    std::vector<float> combFilterSamples3 = combFilter(audioSamples, 0.715f, 5399, N);
-    std::vector<float> combFilterSamples4 = combFilter(audioSamples, 0.697f, 5801, N);
+    std::vector<float> combFilterSamples1 = combFilter(audioSamples, 0.742f, 4799);
+    std::vector<float> combFilterSamples2 = combFilter(audioSamples, 0.733f, 4999);
+    std::vector<float> combFilterSamples3 = combFilter(audioSamples, 0.715f, 5399);
+    std::vector<float> combFilterSamples4 = combFilter(audioSamples, 0.697f, 5801);
 
     // process all the comb filters together(parallel)
     std::vector<float> parallelCombFilterSamples(N);
@@ -86,22 +103,21 @@ int main() {
         parallelCombFilterSamples[i] = combFilterSamples1[i] + combFilterSamples2[i] + combFilterSamples3[i] + combFilterSamples4[i];
     }
 
-    // perform all pass filters(in series)
-    std::vector<float> allPassSamples1 = allPassFilter(parallelCombFilterSamples, 0.136f, 900, N);
-    std::vector<float> allPassSamples2 = allPassFilter(allPassSamples1, 0.136f, 900, N);
+    // perform all pass filters(series)
+    std::vector<float> allPassSamples1 = allPassFilter(parallelCombFilterSamples, 0.7f, 1500);
+    std::vector<float> allPassSamples2 = allPassFilter(allPassSamples1, 0.5f, 2700);
 
-    // NORMALIZE DATA (don't want amplitudes to exceed 1 or -1)
+    // NORMALIZE DATA (amplitudes shouldn't exceed 1 or -1)
         // a. find the max amplitude
-        // b. divide all values by the max amplitude so they go below 1(if the max amplitude is above 1)
     float maxAbsAmp = 0.0f;
-    for (int i = 0; i < N; i++) {
+    for (int i = 0; i < T; i++) {
         if (fabs(allPassSamples2[i] > maxAbsAmp)) {
             maxAbsAmp = fabs(allPassSamples2[i]);
         }
     }
-
+        // b. divide all values by the max amplitude(if the max amplitude is above 1)
     if (maxAbsAmp > 1.0f) {
-        for (int i = 0; i < N; i++) {
+        for (int i = 0; i < T; i++) {
             allPassSamples2[i] = allPassSamples2[i] / maxAbsAmp;
         }
     }
@@ -110,11 +126,11 @@ int main() {
     // write data to WavFile with AudioFile
     AudioFile<float> audioFile;
     audioFile.setNumChannels(1);
-    audioFile.setNumSamplesPerChannel(N);
+    audioFile.setNumSamplesPerChannel(T);
     audioFile.setBitDepth(bitDepth);
     audioFile.setSampleRate(sampleRate);
 
-    for(int i = 0; i < N; i++) {
+    for(int i = 0; i < T; i++) {
         audioFile.samples[0][i] = allPassSamples2[i];
         if (i < 150) {
             std::cout << allPassSamples2[i] << std::endl;
